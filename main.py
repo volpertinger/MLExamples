af5dbb702de0cf82698314bcb519916d41f8034d
Nikolay <merzlovnik@mail.ru>
2023-03-08 23:24:06 +0300

    Only UI

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from UI import UI
-from ML import *
 
 if __name__ == '__main__':
     ui = UI()
     ui.init_gui()
-    # ui.init_demo()
-    #model = MLModel("rf")
-    #dataSet = DataSet("test.csv", "test.csv")
-    #model.teach(dataSet)
